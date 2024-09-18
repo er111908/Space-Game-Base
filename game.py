e9ce9d3da6320307e8eb7e1ff76a55bb06613b50
er111908 <evan.rohrback27@sunrisechristian.org>
2024-09-18 13:55:09 -0500

    space

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import sys
 
 import pygame
+from settings import Settings
+from ship import Ship
 
 class NotSpaceInvaders:
     """Totally *not* a reskinned version of Space Invaders.
@@ -10,9 +12,13 @@ class NotSpaceInvaders:
     def __init__(self):
         """Define what happens when the game starts, and also create game resources."""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1600, 1000))
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode((s))
         pygame.display.set_caption("no officer, this is not a classic arcade game from 1978")
+        self.ship = Ship(self)
+        self.background_color = (13, 10, 45)
+        self.clock = pygame.time.Clock()
 
     def run_game(self):
         """Here's the loop that contains the functions that runs every frame of our game."""
@@ -21,9 +27,11 @@ class NotSpaceInvaders:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            self.screen.fill(self.settings.background_color)
+            self.ship.blitme()
             # Make the most-recently-drawn scene visible (Draw frame to screen)
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Instantiate the main app class and run the game.
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1600
+        self.screen_height = 1000
+        self.background_color = (13, 10, 45)
+        
