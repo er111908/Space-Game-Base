89ae25fe810ea6fb4088a3e97546e85de3b66e41
er111908 <evan.rohrback27@sunrisechristian.org>
2024-09-19 13:39:28 -0500

    game

--- game.py
+++ game.py
@@ -14,24 +14,35 @@ class NotSpaceInvaders:
         pygame.init()
         self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((s))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("no officer, this is not a classic arcade game from 1978")
         self.ship = Ship(self)
-        self.background_color = (13, 10, 45)
         self.clock = pygame.time.Clock()
 
     def run_game(self):
         """Here's the loop that contains the functions that runs every frame of our game."""
         while True:
-            # Watch for mouse and keyboard events (aka interrupts)
+            self._check_events
+            self._draw_frame()
+            self.ship.move()
+            self.clock.tick(60)
+
+    def _draw_frame(self):
+        self.screen.fill(self.settings.background_color)
+        self.ship.blitme()
+# Make the most-recently-drawn scene visible (Draw frame to screen)
+        pygame.display.flip()
+
+    def _check_events(self):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.settings.background_color)
-            self.ship.blitme()
-            # Make the most-recently-drawn scene visible (Draw frame to screen)
-            pygame.display.flip()
-            self.clock.tick(60)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                        self.ship.is_moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                        self.ship.is_moving_right = False
 
 if __name__ == '__main__':
     # Instantiate the main app class and run the game.
--- ship.py
+++ ship.py
@@ -6,6 +6,16 @@ class Ship:
         self.screen_rect = game.screen.get_rect()
         self.image = pygame.image.load('assets/my_spaceship_small/png')
         self.rect = self.image.get_rect()
+#start new ship at bottom middle screen
         self.rect.midbottom = self.screen_rect.midbottom
+        self.is_moving_left = False
+        self.is_moving_right = False
+
+    def move(self):
+        if self.is_moving_left:
+            self.rect.x -= 1
+        if self.is_moving_right:
+            self.rect.x += 1
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
