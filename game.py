832e653c50d0c3fd6859a22f767ea845f838df4f
er111908 <evan.rohrback27@sunrisechristian.org>
2024-09-25 13:43:05 -0500

    game

--- game.py
+++ game.py
@@ -51,7 +51,7 @@ class NotSpaceInvaders:
                 self.ship.is_moving_right = True
             
             if self._check_keydown_events(event, self.settings.fire_bullet_keybinding):
-                self._fire_bullet = True
+                self.is_firing_bullet = True
 
             # Keyup Events
             if self._check_keyup_events(event, self.settings.move_left_keybinding):
@@ -59,7 +59,7 @@ class NotSpaceInvaders:
             if self._check_keyup_events(event, self.settings.move_right_keybinding):
                 self.ship.is_moving_right = False
             if self._check_keyup_events(event, self.settings.fire_bullet_keybinding):
-                self._fire_bullet = False
+                self.is_firing_bullet = False
 
     def _check_keydown_events(self, event, keybinding):
         if event.type == pygame.KEYDOWN:
