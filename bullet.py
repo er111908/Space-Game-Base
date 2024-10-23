b2b0edffacec3ff42a0a83dbb6afd9b8687bb0d0
er111908 <evan.rohrback27@sunrisechristian.org>
2024-10-23 13:36:35 -0500

    space

--- bullet.py
+++ bullet.py
@@ -16,7 +16,7 @@ class Bullet(Sprite):
         #self.height = self.settings.bullet_height
         self.speed = self.settings.bullet_speed
 
-        self.image = pygame.image.load("assets/cripple.png")
+        self.image = pygame.image.load("assets/skullemoji.png")
         self.rect = self.image.get_rect()
         self.rect.midtop = game.ship.rect.midtop
         self.y = float(self.rect.y)
